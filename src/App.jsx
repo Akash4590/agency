f43a4d763f649ec12d04400a7a1d2ff6dcc4190f
Akash4590 <akashjaved4590@gmail.com>
2026-05-13 12:35:57 +0500

    i created the teams sectoion and form section adn call post api from web3 froms

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Hero from './components/Hero'
 import Trustedby from './components/Trustedby'
 import Services from './components/Services'
 import OurWork from './components/OurWork'
+import Teams from './components/Teams'
+import ContactUs from './components/ContactUs'
+import {Toaster} from 'react-hot-toast'
 
 const App = () => {
 
@@ -11,12 +14,15 @@ const [theme, settheme] = useState(localStorage.getItem('theme') ? localStorage.
 
   return (
     <div className='dark:bg-black relative'>
+      <Toaster/>
       <Navbar theme={theme} settheme={settheme}/>
       {/* hero section */}
       <Hero/>
       <Trustedby/>
       <Services />
       <OurWork/>
+      <Teams/>
+      <ContactUs/>
     </div>
   )
 }
